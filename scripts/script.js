37105369cc9d975ef5ce297fd72b55d1278d3cf3
zaitken <zaraaitken18@gmail.com>
2022-12-11 00:54:36 +0000

    fixes bug with 18PM repetition and replaces with incremented times

--- scripts/script.js
+++ scripts/script.js
@@ -9,39 +9,21 @@ currentDayEl.text(currentDay);
 var startOfBusiness = moment(09, 'HH');
 var divEl = $(".container");
 
-//Add/append first row for 9am
-divEl.append("<row class=row></row>");
-var row = $(".row");
-
-//Add/append hour div within first row
-row.append("<div class=hour></div>");
-var hour = $(".hour");
-hour.text(startOfBusiness.format('HHA'));
-
-//Add/append text area within first row
-row.append("<textarea class=text-area></textarea>");
-var textArea = $(".text-area");
-
-//Add/append save button within first row
-row.append("<button class=saveBtn></button>");
-
 // While loop that loops to create 9-5 timeblocks
 // For each loop, generate or build new HTML timeblock row
 
 while (startOfBusiness.hour() < 18) {
-    divEl.append("<row class=new-row></row>");
+    divEl.append("<row class='new-row'>" + startOfBusiness.format('HHA') + "</row>");
     var newRow = $(".new-row");
- 
     startOfBusiness.add(1, 'hours');
-    console.log(startOfBusiness.format('HHA'));
 };
 
 //Add/append hour, textarea and button to each new div
-newRow.append("<div class=new-hour></div>");
 newRow.append("<textarea class=text-area></textarea>");
 newRow.append("<button class=saveBtn></button>");
 
+//Check if timeblock is past, present or future - unfinished
 var newHour = $(".new-hour");
 
-newHour.text(startOfBusiness.format("HHA"));
-console.log(startOfBusiness.format("HHA"));
+let now = moment();
+console.log(now.format());
